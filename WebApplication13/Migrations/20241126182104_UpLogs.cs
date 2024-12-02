aaf9a82eb00e0c1093ab3f48c28d3373100b1827
ucsmdeeeee <157951768+ucsmdeeeee@users.noreply.github.com>
2024-12-02 03:17:29 +0300

    Revert "itog (#1)"
    This reverts commit 594496ac034932e04dc28ab4d726e24ac0f7b6f5.

---
+++ WebApplication13/Migrations/20241126182104_UpLogs.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication13.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LogEntries",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "LogEntries");
+
+            migrationBuilder.RenameTable(
+                name: "LogEntries",
+                newName: "Logs");
+
+            migrationBuilder.RenameColumn(
+                name: "UserAgent",
+                table: "Logs",
+                newName: "Request");
+
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Logs",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<long>(
+                name: "BytesSent",
+                table: "Logs",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusCode",
+                table: "Logs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Logs",
+                table: "Logs",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Logs",
+                table: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "BytesSent",
+                table: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "StatusCode",
+                table: "Logs");
+
+            migrationBuilder.RenameTable(
+                name: "Logs",
+                newName: "LogEntries");
+
+            migrationBuilder.RenameColumn(
+                name: "Request",
+                table: "LogEntries",
+                newName: "UserAgent");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "LogEntries",
+                newName: "Timestamp");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "LogEntries",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "LogEntries",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LogEntries",
+                table: "LogEntries",
+                column: "Id");
+        }
+    }
+}
