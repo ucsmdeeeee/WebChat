38c5a5b3226df2100a067cea64754585129a975c
ucsmdeeeee <157951768+ucsmdeeeee@users.noreply.github.com>
2024-12-02 03:22:30 +0300

    Delete WebApplication13 directory

--- WebApplication13/Controllers/AdminController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-public class AdminController : Controller
-{
-    public IActionResult Logs()
-    {
-        return View();
-    }
-}
--- WebApplication13/Services/ILogWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace WebApplication13.Services
-{
-    public interface ILogWriter
-    {
-        Task WriteLogAsync(string ipAddress, DateTime timestamp, string request, int statusCode, long bytesSent);
-    }
-}
